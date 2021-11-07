9b0ea1d1865f3c2b25610674aec800f077cbc16f
Olive105 <105.olivesoft@gmail.com>
2021-11-07 13:34:33 -0500

    feat - implement createTask feature

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,21 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
   uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  constructor() public {
+    createTask("Check out dappuniversity.com");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
 }
